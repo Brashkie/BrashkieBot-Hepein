107627d9c9d536690055ae37a9806b202dc03a26
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-11 11:22:35 -0500

    BrashkieBot

---
+++ plugins/anime-sad.js
@@ -0,0 +1,23 @@
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} esta llorando`
+    const vi = ['https://telegra.ph/file/d3462edb881db80ea00a8.mp4','https://telegra.ph/file/718c867e5f85b47e545e3.mp4']
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['sad1']
+handler.tags = ['anime']
+export default handler
