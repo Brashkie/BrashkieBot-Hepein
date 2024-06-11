a1a909535a9b204fa558b7ef0a515dd42116db38
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-11 11:23:42 -0500

    BrashkieBot

---
+++ plugins/anime-slap.js
@@ -0,0 +1,23 @@
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} le bofeteó a *@${who.split('@')[0]}*`
+    const vi = ['https://telegra.ph/file/c31d280f53a6002620c5d.mp4','https://telegra.ph/file/ca254155729831888befa.mp4','https://telegra.ph/file/c91ad7b4ec3ffb0b606c6.mp4']
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['slap1']
+handler.tags = ['anime']
+export default handler
