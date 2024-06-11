5ec1d147953b07c9b93fe7a7db6cfcc2c9edbb83
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-11 11:20:11 -0500

    BrashkieBot

---
+++ plugins/anime-kiss.js
@@ -0,0 +1,23 @@
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} le besó a *@${who.split('@')[0]}*`
+    const vi = ['https://telegra.ph/file/63c8cc3cc497e6835b188.mp4','https://telegra.ph/file/9651fa12f8b272afaf364.mp4']
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['kiss1']
+handler.tags = ['anime']
+export default handler
