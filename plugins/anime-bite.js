4fa16988130ad8b909f4f7be5aada76452765030
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-11 11:21:20 -0500

    BrashkieBot

---
+++ plugins/anime-bite.js
@@ -0,0 +1,23 @@
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} le mordió a *@${who.split('@')[0]}*`
+    const vi = ['https://telegra.ph/file/b60d9f51b18c56bdb1599.mp4','https://telegra.ph/file/74e6decdbd7f7240edae0.mp4','https://telegra.ph/file/c384c2f88c7aa800df9a4.mp4']
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['bite1']
+handler.tags = ['anime']
+export default handler
