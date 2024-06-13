2804d8336ecb903d0195eb21505dd771dd3d5fe9
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-12 19:21:07 -0500

    BrashkieBot

---
+++ plugins/anime-food.js
@@ -0,0 +1,25 @@
+/*Fue creado por BrashkieBot*/
+
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} esta comiendo`
+    const vi = ['https://telegra.ph/file/b2b33cfa0774acc0cb44b.mp4']
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['food1']
+handler.tags = ['anime']
+export default handler
