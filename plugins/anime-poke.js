cc622d0b700fd0543b8a890430e80be6bfe93c50
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-13 12:16:18 -0500

    BrashkieBot

--- plugins/anime-poke.js
+++ plugins/anime-poke.js
@@ -1,3 +1,5 @@
+/*Fue creado por BrashkieBot*/
+
 import fs from 'fs'  
 import moment from 'moment-timezone'
 import fetch from 'node-fetch'
@@ -13,7 +15,11 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupM
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let toUser = `${m.sender.split("@")[0]}`
     let menu = `@${toUser} le toca a *@${who.split('@')[0]}*`
-    const vi = ['https://telegra.ph/file/dbc4db37ecd470b90aa40.mp4','https://telegra.ph/file/0da2ae3311426c5317678.mp4']
+    const vi = ['https://telegra.ph/file/dbc4db37ecd470b90aa40.mp4',
+        'https://telegra.ph/file/0da2ae3311426c5317678.mp4',
+        'https://telegra.ph/file/a3e386c4072c916613ee7.mp4',
+        'https://telegra.ph/file/e5d559720b89f1c543626.mp4'
+    ]
     
     await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
 }
