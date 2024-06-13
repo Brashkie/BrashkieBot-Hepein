e86983f5b1a9d25051e7870708fc37a4b10479dd
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-13 18:30:05 -0500

    BrashkieBot

---
+++ plugins/anime-programming.js
@@ -0,0 +1,26 @@
+/*Fue creado por BrashkieBot*/
+
+import fs from 'fs'  
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+import util from 'util'
+import path from 'path' 
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupMetadata, text, command }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let toUser = `${m.sender.split("@")[0]}`
+    let menu = `@${toUser} està programando`
+    const vi = ['https://telegra.ph/file/0f60b734d84525f913191.mp4'
+    ]
+    
+    await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
+}
+
+handler.command = handler.help = ['code']
+handler.tags = ['anime']
+export default handler
