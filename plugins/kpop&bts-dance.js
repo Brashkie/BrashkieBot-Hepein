8cd2790bb836df275fcca4b30f62833853fc69af
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-23 22:42:35 -0500

    BrashkieBot

--- plugins/kpop&bts-dance.js
+++ plugins/kpop&bts-dance.js
@@ -15,12 +15,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, participants, groupM
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let toUser = `${m.sender.split("@")[0]}`
     let menu = `@${toUser} esta bailando XD`
-    const vi = ['https://telegra.ph/file/63c8cc3cc497e6835b188.mp4',
-        'https://telegra.ph/file/9651fa12f8b272afaf364.mp4',
-        'https://telegra.ph/file/e1d573470c7b848ad1c59.mp4',
-        'https://telegra.ph/file/0436ce6db32656eccb2f3.mp4',
-        'https://telegra.ph/file/e58467fd29080d65fc15d.mp4'
-    ]
+    const vi = ['https://telegra.ph/file/e138d1566a48db444d578.mp4']
     
     await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, who] })
 }
