54c519d646ee378d6b9d3d8cf51a9ae86a6125b0
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-06-28 07:13:10 -0500

    BrashkieBot

--- plugins/fun-insultar.js
+++ plugins/fun-insultar.js
@@ -1,16 +1,17 @@
-let handler  = async (m, { conn, usedPrefix,text }) => {
-conn.reply(m.chat,`⬣━━━━[ 🤪 𝗜𝗡𝗦𝗨𝗟𝗧𝗔𝗥 🤪 ]━━━━⬣\n\n`,conn.parseMention(text),`\n*“Eres un ${pickRandom(global.insultar)}”*\n\n⬣━━━━━━[ ${vs} ]━━━━━⬣`, m)}
-
-    
-function pickRandom(list) {
-    return list[Math.floor(list.length * Math.random())]
+let handler = async (m, { conn, usedPrefix, text }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    conn.reply(m.chat, 
+    `⬣━━━━[ 🤪 𝗜𝗡𝗦𝗨𝗟𝗧𝗔𝗥 🤪 ]━━━━⬣\n\n@${who.split('@')[0]}\n*“Eres un ${pickRandom(global.insultar)}”*\n\n⬣━━━━━━[ ${vs} ]━━━━━⬣`, m, {mentions: [m.sender, who]});
 }
     
-global.insultar = ['Abrazafarolas','Adefesio','Adoquín','Alelado','Alfeñique','Andurriasmo','Analfabeto','Apollardao','Archipámpano','Artabán','Asaltacunas','Asno','Atontao'] 
-
-
+function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())];
+}
+    
+global.insultar = ['Abrazafarolas','Adefesio','Adoquín','Alelado','Alfeñique','Andurriasmo','Analfabeto','Apollardao','Archipámpano','Artabán','Asaltacunas','Asno','Atontao'];
+    
 handler.help = ["insultar"];
 handler.tags = ["fun"];
 handler.command = /^(insult|abuse|insultar)$/i;
-handler.fail = null
+handler.fail = null;
 export default handler;
