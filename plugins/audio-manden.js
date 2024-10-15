ef9e69a04e5f067ec5d265177ca3d5dbe6cce807
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-10-15 18:45:27 -0500

    BrashkieBot

---
+++ plugins/audio-manden.js
@@ -0,0 +1,22 @@
+/*Fue creado por BrashkieBot*/
+/*let handler = async (m, { conn }) => {
+    
+    if (m.text.toLowerCase().includes('nopor') || m.text.toLowerCase().includes('porno') || m.text.toLowerCase().includes('hentai') || m.text.toLowerCase().includes('h') || m.text.toLowerCase().includes('ja')) {
+        // Ruta del archivo de audio
+        const audioFilePath = './media/audiobot/concha de tu madre.mp3'; // Reemplaza con la ruta del archivo de audio
+
+        // Envío del archivo de audio como respuesta
+        try {
+            const audioBuffer = await fs.promises.readFile(audioFilePath); // Lee el archivo de audio
+            await conn.sendFile(m.chat, audioBuffer, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true});
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+handler.customPrefix = /nopor|porno|hentai/i;
+handler.command = new RegExp;
+handler.exp = 50;
+
+export default handler;*/
