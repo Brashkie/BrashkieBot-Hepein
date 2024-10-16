5cee305d4a8924f023e030853fe61a8f4e2e3f82
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-10-15 22:41:44 -0500

    BrashkieBot

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -1,7 +1,7 @@
-let ro = 3000
+let ro = 10
 let handler = async (m, { conn, usedPrefix, command}) => {
 let time = global.db.data.users[m.sender].lastrob + 7200000
-if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️¡Hey! Espera ${msToTime(time - new Date())} para volver a robar*`
+if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `> *⏱️¡Hey! Espera ${msToTime(time - new Date())} para volver a robar*`
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 else who = m.chat
@@ -9,12 +9,12 @@ if (!who) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼  
 if (!(who in global.db.data.users)) throw `${lenguajeGB['smsAvisoAG']()}𝙀𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙐𝙀𝙉𝙏𝙍𝘼 𝙀𝙉 𝙈𝙄 𝘽𝘼𝙎𝙀 𝘿𝙀 𝘿𝘼𝙏𝙊𝙎.`
 let users = global.db.data.users[who]
 let rob = Math.floor(Math.random() * ro)
-if (users.exp < rob) return m.reply(`😿 @${who.split`@`[0]} tiene menos de *${ro} XP* No robes a un pobre :v`, null, { mentions: [who] })    
-global.db.data.users[m.sender].exp += rob
-global.db.data.users[who].exp -= rob 
+if (users.limit < rob) return m.reply(`> 👀 @${who.split`@`[0]} tiene menos de *${ro} Diamantes* No robes a un pobre :v`, null, { mentions: [who] },canaloficial)    
+global.db.data.users[m.sender].limit += rob
+global.db.data.users[who].limit -= rob 
 global.db.data.users[m.sender].money += rob
 global.db.data.users[who].money -= rob 
-m.reply(`*✧ Robaste ${rob} Xp a @${who.split`@`[0]}*`, null, { mentions: [who] })
+m.reply(`> *✧ Robaste ${rob} diamante a @${who.split`@`[0]}*`, null, { mentions: [who] },canaloficial)
 global.db.data.users[m.sender].lastrob = new Date * 1
 }
 handler.help = ['rob']
