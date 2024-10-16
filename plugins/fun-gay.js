904232953f42af1e5e73146be9722a4dc313eb4e
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-10-15 19:28:57 -0500

    BrashkieBot

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) => {
+/*let handler = async (m, { conn }) => {
 let vn = './media/a.mp3';
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {  
@@ -10,5 +10,28 @@ ptt: true });
 };
 handler.help = ['gay']
 handler.tags = ['maker']
+handler.command = /^(gay)$/i
+export default handler*/
+
+/*let handler = async (m, {conn}) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*Quien quiere violar a este gay?* 🏳️‍🌈', m)
+}
+handler.help = ['gay *@user*']
+handler.tags = ['fun']
+handler.command = /^(gay)$/i
+export default handler*/
+
+
+let handler = async (m, {conn}) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*Quien quiere violar a este gay?* 🏳️‍🌈', m)
+}
+handler.help = ['gay *@user*']
+handler.tags = ['fun']
 handler.command = /^(gay)$/i
 export default handler
